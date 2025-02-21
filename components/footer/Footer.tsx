132fe82d3d6b5b4f2b27af5ad859acb38e5ee2be
Aaron Damilola <aarondamilola1998@gmail.com>
2025-02-21 12:03:18 +0100

    fix: fixed current year issue

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { getCurrentYear } from '@/lib/utils'
 
 type Props = {}
 
 export default function Footer({}: Props) {
   return (
     <footer className='flex gap-4 justify-between items-center p-4 text-sm'>
-      <p>@InvoFlex2023</p>
+      <p>@InvoFlex{getCurrentYear()}</p>
       <div className='flex gap-1'>
         <p>Privacy policy</p>
         <p>Terms and conditions</p>
